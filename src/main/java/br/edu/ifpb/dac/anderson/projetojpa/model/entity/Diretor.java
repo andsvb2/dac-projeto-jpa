4b2bc646ba442ac92004683331c26f0fef5a57e1
Anderson S. Vieira <svieira.anderson@gmail.com>
2023-08-18 21:45:37 -0300

    cria relação entre diretor e filme

--- src/main/java/br/edu/ifpb/dac/anderson/projetojpa/model/entity/Diretor.java
+++ src/main/java/br/edu/ifpb/dac/anderson/projetojpa/model/entity/Diretor.java
@@ -3,6 +3,12 @@ package br.edu.ifpb.dac.anderson.projetojpa.model.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -14,4 +20,39 @@ public class Diretor {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @OneToMany(mappedBy = "diretor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Filme> filmes = new LinkedHashSet<>();
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "pais")
+    private String pais;
+
+    @Column(name = "data_nascimento")
+    private LocalDate data_nascimento;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Diretor diretor = (Diretor) o;
+        return getId() != null && Objects.equals(getId(), diretor.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "nome = " + nome + ", " +
+                "pais = " + pais + ", " +
+                "data_nascimento = " + data_nascimento.toString() + ")";
+    }
 }
