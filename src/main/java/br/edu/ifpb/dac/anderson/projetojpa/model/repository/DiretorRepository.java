b0ad2ffa8639850807b54875b628725ec3a13076
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-08-21 21:50:25 -0300

    cria DiretorRepository

---
+++ src/main/java/br/edu/ifpb/dac/anderson/projetojpa/model/repository/DiretorRepository.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.anderson.projetojpa.model.repository;
+
+import br.edu.ifpb.dac.anderson.projetojpa.model.entity.Diretor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DiretorRepository extends JpaRepository<Diretor, Long> {
+    List<Diretor> findDiretorByNome(String nome);
+    List<Diretor> findDiretorsByPais(String pais);
+}
--- src/main/java/br/edu/ifpb/dac/anderson/projetojpa/model/repository/FilmeRepository.java
+++ src/main/java/br/edu/ifpb/dac/anderson/projetojpa/model/repository/FilmeRepository.java
@@ -4,11 +4,12 @@ import br.edu.ifpb.dac.anderson.projetojpa.model.entity.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Year;
 import java.util.List;
 
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
     List<Filme> findFilmeByTitulo(String titulo);
-    List<Filme> findFilmeByDiretor(String diretor);
     List<Filme> findFilmeByGenero(String genero);
+    List<Filme> findFilmeByAno(Year ano);
 }
